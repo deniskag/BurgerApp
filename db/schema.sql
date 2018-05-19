37017cd97464c9677275df7146f52bbea19a8cf7
deniskag <deniskagg@gmail.com>
2018-05-19 14:54:57 -0400

    db connection changed

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP database IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+#DROP database IF EXISTS burgers_db;
+#CREATE DATABASE burgers_db;
+#USE burgers_db;
 CREATE TABLE burgers(
 id int NOT NULL auto_increment,
 burger_name VARCHAR (30) NOT NULL,
